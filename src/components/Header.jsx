689409421b12441b62609a997119bf698b0452d8
hactuallybenji <mrbenamiller@gmail.com>
2023-09-16 11:50:57 -0700

    Fixes package.json syntax, and adds basic tailwind header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <>
+    <h1 className="text-center">Joanna Yuen</h1>
+    <nav className="flex justify-center">
+      <ul className="flex">
+        <li className="flex-auto px-10">Social Media</li>
+        <li className="flex-auto px-10">Email Marketing</li>
+        <li className="flex-auto px-10">About</li>
+      </ul>
+    </nav>
+    </>
+  );
+}
+
+export default Header;
