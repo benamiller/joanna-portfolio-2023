4377bf119de86b24f012e40cfdd65acb173fb4d9
Joanna Yuen <joanna@Joannas-Air.hitronhub.home>
2023-09-17 20:19:14 -0700

    adds report/proposal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
     <nav className="flex justify-center">
       <ul className="flex">
         <li className="flex-auto px-10">Social Media</li>
+        <li className="flex-auto px-10">Reports/Proposals</li>
         <li className="flex-auto px-10">Email Marketing</li>
         <li className="flex-auto px-10">About</li>
       </ul>
