0b24fc0e438089ea3b38fe9ad535b6a3e0be8580
hactuallybenji <mrbenamiller@gmail.com>
2023-09-23 11:46:54 -0700

    Adds storybook dependency and configuration with tailwind

---
+++ src/stories/MyHeader.stories.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header.jsx';
+
+export default {
+  title: 'Example/MyHeader',
+  component: Header,
+
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Primary = {};
